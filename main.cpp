a8e11481d63464228db6825059bd0ccccf3a5224
Maxim Kokryashkin <max.kokryashkin@gmail.com>
2020-05-12 03:26:45 +0300

    reading bmp header

--- main.cpp
+++ main.cpp
@@ -7,7 +7,12 @@
 #include <immintrin.h>
 #include <smmintrin.h>
 
-const int BMP_HEADER_END = 0x8a;
+const int BMP_FILE_SIZE_OFFSET = 0x2;
+const int BMP_FILE_OFFBITS_OFFSET = 0xA;
+const int BMP_FILE_WIDTH_OFFSET = 0x12;
+const int BMP_FILE_HEIGHT_OFFSET = 0x16;
+
+
 const int BYTES_PER_PIXEL = 4;
 const unsigned char MAX_ALPHA = 255;
 const int MAX_ALPHA_POW = 8;
@@ -15,8 +20,16 @@ const int MAX_ALPHA_POW = 8;
 
 class BMPFile {
     private:
-        std::unique_ptr<unsigned char> data_;
-        long long size_ = 0;
+        std::unique_ptr<unsigned char[]> data_;
+        int size_ = 0;
+        int height_ = 0;
+        int width_ = 0;
+        int pixel_data_start_ = 0;
+
+        void ReadProperty(int& property, int offset, FILE* file) {
+            fseek(file, offset, SEEK_SET);
+            fread(&property, sizeof(int), 1, file);
+        }
 
         class FileCloser {
             public:
@@ -35,13 +48,19 @@ class BMPFile {
 
         BMPFile(const BMPFile& other) {
             size_ = other.size_;
-            data_ = std::unique_ptr<unsigned char>(new unsigned char[other.size_]());
+            data_ = std::unique_ptr<unsigned char[]>(new unsigned char[other.size_]());
+            height_ = other.height_;
+            width_ = other.width_;
+            pixel_data_start_ = other.pixel_data_start_;
             memcpy(data_.get(), other.data_.get(), size_);   
         }
 
         BMPFile& operator=(const BMPFile& other) {
             size_ = other.size_;
-            data_ = std::unique_ptr<unsigned char>(new unsigned char[other.size_]());
+            height_ = other.height_;
+            width_ = other.width_;
+            pixel_data_start_ = other.pixel_data_start_;
+            data_ = std::unique_ptr<unsigned char[]>(new unsigned char[other.size_]());
             memcpy(data_.get(), other.data_.get(), size_);
             
             return *this;
@@ -63,16 +82,30 @@ class BMPFile {
                 throw std::runtime_error("This file does not exist!");
             }
             
-            fseek(bmp_file.get(), 0, SEEK_END);
-            size_ = ftell(bmp_file.get());
-            fseek(bmp_file.get(), 0, SEEK_SET);
+            ReadProperty(size_, BMP_FILE_SIZE_OFFSET, bmp_file.get());
+            ReadProperty(width_, BMP_FILE_WIDTH_OFFSET, bmp_file.get());
+            ReadProperty(height_, BMP_FILE_HEIGHT_OFFSET, bmp_file.get());
+            ReadProperty(pixel_data_start_, BMP_FILE_OFFBITS_OFFSET, bmp_file.get());
 
-            data_ = std::unique_ptr<unsigned char>(new unsigned char[size_]());
+            fseek(bmp_file.get(), 0, SEEK_SET);
+            data_ = std::unique_ptr<unsigned char[]>(new unsigned char[size_]());
             fread(data_.get(), sizeof(unsigned char), size_, bmp_file.get());
         }
 
-        long long Size() const noexcept {
+        int Size() const noexcept {
             return size_;
+        }
+
+        int Width() const noexcept {
+            return width_;
+        }
+
+        int Height() const noexcept {
+            return height_;
+        }
+
+        int OffsetPixels() const noexcept {
+            return pixel_data_start_;
         }
 
         const unsigned char* Data() const noexcept {
@@ -89,7 +122,7 @@ class BMPFile {
                 throw std::runtime_error("Both pictures must be the same size!");
             }
 
-            for(int i = BMP_HEADER_END; i < size_; i += BYTES_PER_PIXEL){
+            for(int i = pixel_data_start_; i < size_; i += BYTES_PER_PIXEL){
                 unsigned char src_alpha = other.data_.get()[i + 3];
 
                 int* dest_pixel_pointer = reinterpret_cast<int*>(data_.get() + i);
@@ -124,6 +157,9 @@ class BMPFile {
         friend void swap(BMPFile& first, BMPFile& second) noexcept {
             std::swap(first.data_, second.data_);
             std::swap(first.size_, second.size_);
+            std::swap(first.height_, second.height_);
+            std::swap(first.width_, second.width_);
+            std::swap(first.pixel_data_start_, second.pixel_data_start_);
         }
     
 };
@@ -133,5 +169,10 @@ int main() {
     auto book_file = BMPFile("pictures/book.bmp");
     cat_file.ComposeAlpha(book_file);
     cat_file.SaveToFile("pictures/composed.bmp");
+    std::cout << "height: " << cat_file.Height() << std::endl;
+    std::cout << "width: " << cat_file.Width() << std::endl;
+    std::cout << "size: " << cat_file.Size() << std::endl;
+    std::cout << "offset: " << cat_file.OffsetPixels() << std::endl;
+
     return 0;
 }
